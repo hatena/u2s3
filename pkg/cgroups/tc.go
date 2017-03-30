9edfd5422d0208682a48b206e47f20add0c91cfa
taku-k <taakuu19@gmail.com>
2017-03-30 18:03:08 +0900

    Fix a bug that tc is not enabled

--- pkg/cgroups/tc.go
+++ pkg/cgroups/tc.go
@@ -5,6 +5,7 @@ import (
 	"os/exec"
 
 	"github.com/taku-k/log2s3-go/pkg"
+	"strings"
 )
 
 const (
@@ -12,18 +13,18 @@ const (
 )
 
 func createLimitBW(c *pkg.UploadConfig, minor int) error {
-	cmd := fmt.Sprintf("tc qdisc del dev %s root", c.Device)
-	_ = exec.Command(cmd).Run()
-	cmd = fmt.Sprintf("tc qdisc add dev %s root handle %d: htb", c.Device, MAJOR_ID)
-	if err := exec.Command(cmd).Run(); err != nil {
+	args := strings.Split(fmt.Sprintf("qdisc del dev %s root", c.Device), " ")
+	_ = exec.Command("tc", args...).Run()
+	args = strings.Split(fmt.Sprintf("qdisc add dev %s root handle %d: htb", c.Device, MAJOR_ID), " ")
+	if err := exec.Command("tc", args...).Run(); err != nil {
 		return err
 	}
-	cmd = fmt.Sprintf("tc class add dev %s parent %d: classid %d:%d htb rate %dmbit", c.Device, MAJOR_ID, MAJOR_ID, minor, c.RateLimit)
-	if err := exec.Command(cmd).Run(); err != nil {
+	args = strings.Split(fmt.Sprintf("class add dev %s parent %d: classid %d:%d htb rate %dmbit", c.Device, MAJOR_ID, MAJOR_ID, minor, c.RateLimit), " ")
+	if err := exec.Command("tc", args...).Run(); err != nil {
 		return err
 	}
-	cmd = fmt.Sprintf("tc filter add dev %s parent %d: protocol ip prio %d handle 1: cgroup", c.Device, MAJOR_ID, MAJOR_ID)
-	if err := exec.Command(cmd).Run(); err != nil {
+	args = strings.Split(fmt.Sprintf("filter add dev %s parent %d: protocol ip prio %d handle 1: cgroup", c.Device, MAJOR_ID, MAJOR_ID), " ")
+	if err := exec.Command("tc", args...).Run(); err != nil {
 		return err
 	}
 	return nil
