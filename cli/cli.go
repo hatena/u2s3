c183480233c87863d73145cb2261c7897ac4bb7f
taku-k <taakuu19@gmail.com>
2017-04-06 12:21:05 +0900

    Remove upload subcmd and add upload-{log,file} subcmd

--- cli/cli.go
+++ cli/cli.go
@@ -9,27 +9,35 @@ import (
 
 var globalFlags = []cli.Flag{}
 
+var commonFlags = []cli.Flag{
+	cli.StringFlag{Name: "f, file", Usage: "input file"},
+	cli.StringFlag{Name: "o, output", Usage: "directory for output"},
+	cli.StringFlag{Name: "kf, key-format", Value: "{{.Output}}/{{.Year}}/{{.Month}}/{{.Day}}/{{.Hostname}}-{{.Year}}{{.Month}}{{.Day}}{{.Hour}}{{.Minute}}_{{.Seq}}.log.gz", Usage: "object key format"},
+	cli.StringFlag{Name: "b, bucket", Usage: "bucket name"},
+	cli.IntFlag{Name: "m, max-retry", Value: 5, Usage: "limit retry times"},
+	cli.IntFlag{Name: "cpu", Usage: "cpu usage limitation (%)"},
+	cli.IntFlag{Name: "memory", Usage: "memory usage limitation (MB)"},
+	cli.IntFlag{Name: "rate", Usage: "bandwidth rate limit (MB)"},
+	cli.StringFlag{Name: "dev", Value: "eth0", Usage: "rate limit device (default: eth0)"},
+}
+
 var commands = []cli.Command{
 	{
-		Name:   "upload",
+		Name:   "upload-log",
 		Usage:  "Upload a log file to S3",
-		Action: uploadCmd,
-		Flags: []cli.Flag{
-			cli.StringFlag{Name: "f, file", Usage: "input file"},
+		Action: uploadLogCmd,
+		Flags: append(commonFlags,
 			cli.IntFlag{Name: "s, step", Value: 30, Usage: "step for splitting logs. (min)"},
-			cli.StringFlag{Name: "p, prefix", Usage: "prefix of splitted logs"},
-			cli.StringFlag{Name: "o, output", Usage: "directory for output"},
 			cli.StringFlag{Name: "l, log-format", Value: "tsv", Usage: "tsv or ssv (default: tsv)"},
-			cli.StringFlag{Name: "ok, output-key", Value: "{{.Output}}/{{.Year}}/{{.Month}}/{{.Day}}/{{.Hostname}}-{{.Year}}{{.Month}}{{.Day}}{{.Hour}}{{.Minute}}_{{.Seq}}.log.gz", Usage: "object key format"},
-			cli.StringFlag{Name: "b, bucket", Usage: "bucket name"},
-			cli.IntFlag{Name: "m, max-retry", Value: 5, Usage: "the maximum number of retry times"},
-			cli.IntFlag{Name: "cpu", Usage: "cpu usage limitation (%)"},
-			cli.IntFlag{Name: "memory", Usage: "memory usage limitation (MB)"},
-			cli.IntFlag{Name: "rate", Usage: "bandwidth rate limit (MB)"},
-			cli.StringFlag{Name: "dev", Value: "eth0", Usage: "rate limit device (default: eth0)"},
-			cli.BoolFlag{Name: "ca, content-aware", Usage: "Content-aware uploading is enabled"},
+		),
+	},
+	{
+		Name:   "upload-file",
+		Usage:  "Upload a file to S3",
+		Action: uploadCmd,
+		Flags: append(commonFlags,
 			cli.StringFlag{Name: "ff, filename-format", Usage: "file name format e.g. " + `(?<Year>\d{4})-(?<Month>\d{2})-(?<Day>\d{2})`},
-		},
+		),
 	},
 }
 
@@ -40,7 +48,7 @@ func commandNotFound(c *cli.Context, command string) {
 
 func Main() {
 	app := cli.NewApp()
-	app.Name = "log2s3"
+	app.Name = "u2s3"
 	app.Version = "0.1.0"
 	app.Author = "taku-k"
 	app.Email = "taakuu19@gmail.com"
--- cli/upload.go
+++ cli/upload.go
@@ -11,42 +11,30 @@ import (
 	"github.com/urfave/cli"
 )
 
-func uploadCmd(c *cli.Context) error {
+func uploadLogCmd(c *cli.Context) error {
 	cfg := &pkg.UploadConfig{
-		FileName:        c.String("file"),
-		LogFormat:       c.String("log-format"),
-		KeyFormat:       c.String("output-key"),
-		OutputPrefixKey: c.String("output"),
 		Step:            c.Int("step"),
+		LogFormat:       c.String("log-format"),
+		FileName:        c.String("file"),
+		KeyFormat:       c.String("key-format"),
+		OutputPrefixKey: c.String("output"),
 		Bucket:          c.String("bucket"),
 		MaxRetry:        c.Int("max-retry"),
 		CPULimit:        c.Int("cpu"),
 		MemoryLimit:     c.Int("memory"),
 		RateLimit:       c.Int("rate"),
 		Device:          c.String("dev"),
-		ContentAware:    c.Bool("content-aware"),
-		FilenameFormat:  c.String("filename-format"),
 	}
 
 	pp.Println(cfg)
 
-	cmngr, err := resourcelimit.NewCgroupMngr(cfg)
-	if err == nil {
-		defer cmngr.Close()
-	} else {
-		fmt.Println(err)
-	}
+	initResourceLimit(cfg)
 
 	if cfg.Bucket == "" {
 		return errors.New("Bucket name must be specified")
 	}
 
-	var agg core.Aggregator
-	if cfg.ContentAware {
-		agg, err = core.NewEpochAggregator(cfg)
-	} else {
-		agg, err = core.NewFileAggregator(cfg)
-	}
+	agg, err := core.NewEpochAggregator(cfg)
 	if err != nil {
 		return err
 	}
@@ -61,4 +49,52 @@ func uploadCmd(c *cli.Context) error {
 		}
 	}
 	return nil
+}
+
+func uploadCmd(c *cli.Context) error {
+	cfg := &pkg.UploadConfig{
+		FilenameFormat:  c.String("filename-format"),
+		FileName:        c.String("file"),
+		KeyFormat:       c.String("key-format"),
+		OutputPrefixKey: c.String("output"),
+		Bucket:          c.String("bucket"),
+		MaxRetry:        c.Int("max-retry"),
+		CPULimit:        c.Int("cpu"),
+		MemoryLimit:     c.Int("memory"),
+		RateLimit:       c.Int("rate"),
+		Device:          c.String("dev"),
+	}
+
+	pp.Println(cfg)
+
+	initResourceLimit(cfg)
+
+	if cfg.Bucket == "" {
+		return errors.New("Bucket name must be specified")
+	}
+
+	agg, err := core.NewFileAggregator(cfg)
+	if err != nil {
+		return err
+	}
+	defer agg.Close()
+	if err := agg.Run(); err != nil {
+		return err
+	}
+	up := core.NewUploader(cfg)
+	for _, f := range agg.GetUploadableFiles() {
+		if err := up.Upload(f); err != nil {
+			return err
+		}
+	}
+	return nil
+}
+
+func initResourceLimit(cfg *pkg.UploadConfig) {
+	cmngr, err := resourcelimit.NewCgroupMngr(cfg)
+	if err == nil {
+		defer cmngr.Close()
+	} else {
+		fmt.Println(err)
+	}
 }
