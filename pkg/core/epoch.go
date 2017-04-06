e78160aa6b4cb2d81d1cda5f62a0310dc33d1712
taku-k <taakuu19@gmail.com>
2017-04-06 12:54:32 +0900

    Refactor aggregator

--- pkg/core/epoch.go
+++ pkg/core/epoch.go
@@ -3,12 +3,15 @@ package core
 import (
 	"bufio"
 	"fmt"
+	"io"
 	"io/ioutil"
 	"os"
+	"regexp"
 	"time"
 
 	gzip "github.com/klauspost/pgzip"
 	"github.com/taku-k/u2s3/pkg"
+	"github.com/taku-k/u2s3/pkg/input/content"
 	"github.com/taku-k/u2s3/pkg/util"
 )
 
@@ -23,6 +26,101 @@ type Epoch struct {
 
 type EpochManager struct {
 	epochs map[string]*Epoch
+}
+
+var reTsv = regexp.MustCompile(`(?:^|[ \t])time\:([^\t]+)`)
+
+type EpochAggregator struct {
+	reader content.BufferedReader
+	mngr   *EpochManager
+	config *pkg.UploadConfig
+}
+
+func NewEpochAggregator(cfg *pkg.UploadConfig) (Aggregator, error) {
+	mngr := NewEpochManager()
+	var reader content.BufferedReader
+	var err error
+	if cfg.FileName != "" {
+		reader, err = content.NewFileReader(cfg.FileName)
+		if err != nil {
+			return nil, err
+		}
+	} else {
+		reader = content.NewStdinReader()
+	}
+	return &EpochAggregator{
+		reader: reader,
+		mngr:   mngr,
+		config: cfg,
+	}, nil
+}
+
+func (a *EpochAggregator) Run() error {
+	for {
+		l, err := a.reader.Readln()
+		if err == io.EOF {
+			break
+		} else if err != nil {
+			return err
+		}
+		epochKey := parseEpoch(string(l), a.config.LogFormat, a.config.Step)
+		if epochKey == "" {
+			continue
+		}
+		var epoch *Epoch
+		if !a.mngr.HasEpoch(epochKey) {
+			epoch, err = NewEpoch(epochKey, a.config.KeyFormat, a.config.OutputPrefixKey)
+			if err != nil {
+				return err
+			}
+			a.mngr.PutEpoch(epoch)
+		} else {
+			epoch = a.mngr.GetEpoch(epochKey)
+		}
+		epoch.Write(l)
+	}
+	return nil
+}
+
+func (a *EpochAggregator) GetUploadableFiles() []UploadableFile {
+	v := make([]UploadableFile, 0, len(a.mngr.epochs))
+	for _, e := range a.mngr.epochs {
+		v = append(v, e)
+	}
+	return v
+}
+
+func (a *EpochAggregator) Close() {
+	a.reader.Close()
+	a.mngr.Close()
+}
+
+func parseEpoch(l, logFormat string, step int) string {
+	r := ""
+	switch logFormat {
+	case "ssv":
+		break
+	case "tsv":
+		m := reTsv.FindStringSubmatch(l)
+		if len(m) == 2 {
+			r = m[1]
+		}
+		if len(r) >= 2 {
+			if r[0] == '[' && r[len(r)-1] == ']' {
+				r = r[1 : len(r)-1]
+			}
+		}
+		break
+	}
+	if r == "" {
+		return ""
+	}
+	t, err := time.Parse("02/Jan/2006:15:04:05 -0700", r)
+	if err != nil {
+		return ""
+	}
+	e := time.Unix(t.Unix()-t.Unix()%(int64(step)*60), 0)
+	return e.In(t.Location()).Format("20060102150405")
 }
 
 func NewEpoch(epochKey, keyFmt, output string) (*Epoch, error) {
--- pkg/core/epoch_test.go
+++ pkg/core/epoch_test.go
@@ -31,3 +31,23 @@ func TestGetObjectKey(t *testing.T) {
 		}
 	}
 }
+
+func TestParseEpoch(t *testing.T) {
+	etests := []struct {
+		desc string
+		in   string
+		out  string
+	}{
+		{"base", "time:24/Feb/2017:10:00:07 +0900\thost:127.0.0.1", "20170224100000"},
+		{"just before begin carried", "time:24/Feb/2017:10:29:59 +0900\thost:127.0.0.1", "20170224100000"},
+		{"just after begin carried", "time:24/Feb/2017:10:30:00 +0900\thost:127.0.0.1", "20170224103000"},
+		{"time is after host", "host:127.0.0.1\ttime:24/Feb/2017:10:00:00 +0900", "20170224100000"},
+		{"time is closed in the bracket", "host:127.0.0.1\ttime:[24/Feb/2017:10:00:00 +0900]", "20170224100000"},
+	}
+	for _, tt := range etests {
+		s := parseEpoch(tt.in, "tsv", 30)
+		if s != tt.out {
+			t.Errorf("%q: parseEpoch(%q) => %s, wants %q", tt.desc, tt.in, s, tt.out)
+		}
+	}
+}
