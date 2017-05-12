2fde41f1f48aa221297dc44346afcb8a5604cb09
taku-k <taakuu19@gmail.com>
2017-05-12 19:16:47 +0900

    Add vendor and update .gitignore

---
+++ vendor/github.com/taku-k/cgroups/paths_test.go
@@ -0,0 +1,45 @@
+package cgroups
+
+import (
+	"path/filepath"
+	"strings"
+	"testing"
+)
+
+func TestStaticPath(t *testing.T) {
+	path := StaticPath("test")
+	p, err := path("")
+	if err != nil {
+		t.Fatal(err)
+	}
+	if p != "test" {
+		t.Fatalf("expected static path of \"test\" but received %q", p)
+	}
+}
+
+func TestSelfPath(t *testing.T) {
+	paths, err := parseCgroupFile("/proc/self/cgroup")
+	if err != nil {
+		t.Fatal(err)
+	}
+	dp := strings.TrimPrefix(paths["devices"], "/")
+	path := NestedPath("test")
+	p, err := path("devices")
+	if err != nil {
+		t.Fatal(err)
+	}
+	if p != filepath.Join("/", dp, "test") {
+		t.Fatalf("expected self path of %q but received %q", filepath.Join("/", dp, "test"), p)
+	}
+}
+
+func TestRootPath(t *testing.T) {
+	p, err := RootPath(Cpu)
+	if err != nil {
+		t.Error(err)
+		return
+	}
+	if p != "/" {
+		t.Errorf("expected / but received %q", p)
+	}
+}
